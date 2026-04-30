a10a910b90b13ee6202b81d1e0cacfdd58baf334
Alex S. <Le4ked@gmail.com>
2026-04-30 07:43:12 +0200

    Test 2 Reset

--- stow.sh
+++ stow.sh
@@ -6,14 +6,15 @@ sudo pacman -S --needed stow
 DOTFILES_DIR="$HOME/dotfiles"
 
 packages=(nvim yazi zellij ghostty zsh)
-external_dependencies=(inxi ripgrep wget luarocks cmake tree-sitter-cli nodejs npm python-pynvim lazygit)
+external_dependencies=(inxi ripgrep wget luarocks cmake tree-sitter-cli nodejs npm python-pynvim lazygit manpages)
 sudo pacman -S --needed "${packages[@]}"
 sudo pacman -S --needed "${external_dependencies[@]}"
 
+echo "Commence stowing:"
 for pkg in "${packages[@]}"; do
   echo "Stowing $pkg..."
   stow -d "$DOTFILES_DIR" -t "$HOME" "$pkg"
 done
 
-echo "Remember to use lazygit"
 echo "Done!"
+echo "Remember to use lazygit"
